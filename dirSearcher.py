1a74e1982f957021309e4b6caf934eeb1536cd40
Emin Yıldız <108903009+NorthNo@users.noreply.github.com>
2023-09-11 11:41:27 +0300

    Add files via upload

--- dirSearcher.py
+++ dirSearcher.py
@@ -12,12 +12,12 @@ def printActivePanels():
     print(url)
 
 def transferWordlist(wordlist):
-  wl = open(wordlist, "r")
-
-  for line in wl:
-    line = line.replace(" ", "")
-    line = line.replace("\n", "")
-    wordlistArray.append(line)
+  with open(wordlist, "r") as wl:
+    for line in wl:
+      line = line.strip()
+      
+      if not line.startswith("#"):
+        wordlistArray.append(line)
 
   return len(wordlistArray)
 
@@ -26,8 +26,7 @@ def searcher(url, item):
   req = requests.get(url + line).status_code
     
   if req != 200:
-    pass
-    #print("[{}{}]".format(url, line), "=", "\x1b[34m[STATUS_CODE : {}]\x1b[0m".format(req))
+    print("[{}{}]".format(url, line), "=", "\x1b[34m[STATUS_CODE : {}]\x1b[0m".format(req))
   else:
     print("[{}{}]".format(url, line), "=", "\x1b[31;43;4m[STATUS_CODE : {}]\x1b[0m".format(req))
     okResp.append(f"[{url}{line}] = [STATUS_CODE : {req}]")
